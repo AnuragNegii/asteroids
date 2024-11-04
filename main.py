b08840b09f2bfdc35022fb40deffc411bb93659b
Anurag <Negianurag99@gmail.com>
2024-11-04 12:21:27 +0530

    Added asteroid to the game

--- main.py
+++ main.py
@@ -2,14 +2,30 @@ import pygame
 from pygame.color import Color
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():    
     pygame.init()
+
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    #clock for the game 
     clock = pygame.time.Clock()
-    x = SCREEN_WIDTH/2
-    y = SCREEN_HEIGHT/2
-    player = Player(x, y)
+
+    #containers
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers= (updatable, drawable, asteroid)
+    AsteroidField.containers = (updatable)
+
+    #Player instance created
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroid_field_object = AsteroidField()
+    
+    #asteroids created
+    
     dt = 0
     while True:
         for event in pygame.event.get():
@@ -17,8 +33,12 @@ def main():
                 return
 
         screen.fill(Color(0,0,0))
-        player.draw(screen)
-        player.update(dt)
+
+        for objects in updatable:
+            objects.update(dt)
+        for objects in drawable:
+            objects.draw(screen)
+
         pygame.display.flip()
         #limit the framerate to 60fps
         dt = clock.tick(60)/1000
