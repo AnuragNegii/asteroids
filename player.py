2f1a184334437fd47aeebc2201180af3d76e2818
Anurag <Negianurag99@gmail.com>
2024-11-05 09:43:40 +0530

    added shoot timer so that it waits 0.3s before shooting

--- player.py
+++ player.py
@@ -5,50 +5,55 @@ from constants import *
 
 
 class Player(CircleShape):
-     def __init__(self, x, y):
-          super().__init__(x,y,PLAYER_RADIUS)
-          self.rotation = 0
+    def __init__(self, x, y):
+        super().__init__(x,y,PLAYER_RADIUS)
+        self.rotation = 0
+        self.shoot_timer = 0
 
-#in the player class
-     def triangle(self):
-          forward = pygame.Vector2(0, 1).rotate(self.rotation)
-          right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-          a = self.position + forward * self.radius
-          b = self.position - forward * self.radius - right
-          c = self.position - forward * self.radius + right
-          return [a, b, c]
+    #in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
 
-     def draw(self, screen):
-          pygame.draw.polygon(screen,"white", self.triangle(), 2)
-     
-     def rotate(self, dt):
-          self.rotation += dt * PLAYER_TURN_SPEED
+    def draw(self, screen):
+        pygame.draw.polygon(screen,"white", self.triangle(), 2)
 
-     def update(self, dt):
-          keys = pygame.key.get_pressed()
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
 
-          if keys[pygame.K_a]:
-               self.rotate(-dt) 
-          if keys[pygame.K_d]:
-               self.rotate(dt)
-          if keys[pygame.K_w]:
-               self.move(dt)
-          if keys[pygame.K_s]:
-               self.move(-dt)
-          if keys[pygame.K_SPACE]:
-               self.shoot()
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        print(self.shoot_timer)
+        self.shoot_timer -= dt
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt) 
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
 
-     def move(self, dt):
-          forward = pygame.Vector2(0, 1).rotate(self.rotation)
-          self.position += forward * PLAYER_SPEED * dt
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
-     def shoot(self):
-          gun_shot = Shot(self.position.x, self.position.y)
-          gun_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    def shoot(self):
+        if(self.shoot_timer <= 0):
+            self.shoot_timer = PLAYER_SHOOT_COOLDOWN 
+            gun_shot = Shot(self.position.x, self.position.y)
+            gun_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
-     
-     #    def __add__(self, other): x= (self.position.x + other.position.x)
+
+    #    def __add__(self, other): x= (self.position.x + other.position.x)
 #     y = (self.position.y + other.position.y)
 #     return pygame.Vector2(x, y)
 # 
